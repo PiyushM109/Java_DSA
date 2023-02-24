33253ce4890175ce428f5508c4a54bfd804e8089
Piyush More <piyushmore109gmail.com>
2023-02-24 22:25:26 +0530

    DSA STACK

--- Stack/valid_Pranthesis.java
+++ Stack/valid_Pranthesis.java
@@ -1,3 +1,39 @@
+import java.util.*;
+
 public class valid_Pranthesis {
-    
+    public static boolean paranthesis_Validation(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '{' || str.charAt(i) == '[' || str.charAt(i) == '(') {
+                s.push(str.charAt(i));
+
+            } else {
+                if (s.isEmpty()) {
+                    return false;
+                }
+
+                else if (s.peek() == '(' && str.charAt(i) == ')') {
+                    s.pop();
+                } else if (s.peek() == '{' && str.charAt(i) == '}') {
+                    s.pop();
+                } else if (s.peek() == '[' && str.charAt(i) == ']') {
+                    s.pop();
+                } else {
+                    s.push(str.charAt(i));
+                }
+            }
+        }
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        String str = "{[()]}";
+        System.out.println(paranthesis_Validation(str));
+    }
 }
